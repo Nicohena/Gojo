e79df8de29509fec0858c62dae934d48bfac1261
Nicohena <192819099+Nicohena@users.noreply.github.com>
2026-03-07 14:07:17 +0300

    apply new visual theme and styling updates to dashboard and search components.

--- client/src/components/layout/DashboardLayout.jsx
+++ client/src/components/layout/DashboardLayout.jsx
@@ -4,11 +4,11 @@ import { Navbar } from "./Navbar";
 
 export const DashboardLayout = ({ children }) => {
   return (
-    <div className="flex min-h-screen bg-white">
+    <div className="flex min-h-screen bg-[#0a0a0a]">
       <Sidebar />
       <div className="flex-1 flex flex-col">
         <Navbar />
-        <main className="flex-1 p-8 bg-slate-50/50">{children}</main>
+        <main className="flex-1 p-8 bg-[#0a0a0a]">{children}</main>
       </div>
     </div>
   );
